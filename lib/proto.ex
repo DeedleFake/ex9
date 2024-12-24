bdb0e8934f10800dc74f2457bc1fe39130243448
DeedleFake <deedlefake@users.noreply.github.com>
2024-12-24 17:19:49 -0500

    clean up some stuff and add some typespecs

--- lib/proto.ex
+++ lib/proto.ex
@@ -1,6 +1,7 @@
 defmodule Ex9P.Proto do
   @moduledoc """
-  A single 9P message and related functionality.
+  This module defines a behaviour for serializing and deserializing
+  messages as well as functions to perform those operations.
   """
 
   import Bitwise
@@ -12,20 +13,19 @@ defmodule Ex9P.Proto do
     end
   end
 
-  @type serialized_type() :: pos_integer()
-  @type serialized_data() :: binary()
-  @type message() :: struct()
-
-  @callback deserialize(serialized_type(), serialized_data()) :: message()
-  @callback serialize(message()) :: {serialized_type(), serialized_data()}
+  @callback deserialize(type_id, data) :: message
+            when type_id: pos_integer(), data: binary(), message: struct()
+  @callback serialize(message) :: {type_id, data}
+            when message: struct(), type_id: pos_integer(), data: iodata()
 
   @notag (1 <<< 16) - 1
 
-  def deserialize_message(data, opts \\ [])
-
-  def deserialize_message(<<size::4*8-little, type::8, tag::2*8-little, rest::binary>>, opts) do
+  @spec deserialize_message(message_data, options) :: {message, rest}
+        when message_data: iodata(), options: keyword(), message: struct(), rest: binary()
+  def deserialize_message(data, opts \\ []) do
     %{proto: proto} = Keyword.validate!(opts, proto: Ex9P.Nine) |> Map.new()
 
+    <<size::4*8-little, type::8, tag::2*8-little, rest::binary>> = IO.iodata_to_binary(data)
     datasize = size - 4 - 1 - 2
     <<data::binary-(^datasize * 8), rest::binary>> = rest
     msg = {tag, proto.deserialize(type, data)}
@@ -33,10 +33,8 @@ defmodule Ex9P.Proto do
     {msg, rest}
   end
 
-  def deserialize_message(data, opts) when is_list(data) do
-    deserialize_message(IO.iodata_to_binary(data), opts)
-  end
-
+  @spec serialize_message(message, options) :: message_data
+        when message: struct(), options: keyword(), message_data: iodata()
   def serialize_message(msg, opts \\ [])
 
   def serialize_message({tag, msg}, opts) do
@@ -51,19 +49,24 @@ defmodule Ex9P.Proto do
     serialize_message({@notag, msg}, opts)
   end
 
+  @spec deserialize_binary(data) :: {deserialized_data, rest}
+        when data: iodata(), deserialized_data: binary(), rest: binary()
   def deserialize_binary(<<size::2*8-little, data::binary>>) do
-    <<data::(^size)*8-binary, rest::binary>> = data
+    <<data::(^size)*8-binary, rest::binary>> = IO.iodata_to_binary(data)
     {data, rest}
   end
 
-  def deserialize_binary(data) when is_list(data) do
-    deserialize_binary(IO.iodata_to_binary(data))
-  end
-
+  @spec serialize_binary(data) :: serialized_data when data: iodata(), serialized_data: iodata()
   def serialize_binary(data) do
     [<<IO.iodata_length(data)::2*8-little>>, data]
   end
 
+  @spec deserialize_list(data, deserialize_element) :: {list, rest}
+        when data: iodata(),
+             deserialize_element: (binary() -> {element, binary()}),
+             list: [element],
+             rest: binary(),
+             element: term()
   def deserialize_list(data, deserialize_element)
       when is_binary(data) and is_function(deserialize_element, 1) do
     {data, rest} = deserialize_binary(data)
@@ -77,6 +80,11 @@ defmodule Ex9P.Proto do
     deserialize_list_loop(rest, deserialize_element, [element | result])
   end
 
+  @spec serialize_list(list, serialize_element) :: serialized_list
+        when list: [element],
+             serialize_element: (element -> iodata()),
+             serialized_list: iodata(),
+             element: term()
   def serialize_list(list, serialize_element) when is_function(serialize_element, 1) do
     data =
       for element <- list, reduce: [] do
@@ -84,8 +92,5 @@ defmodule Ex9P.Proto do
       end
 
     [<<IO.iodata_length(data)::2*8-little>> | data]
-  end
-
-  defmodule Proto do
   end
 end
