97391647ba4ecea6a1f5ad658c51a72638a145c8
DeedleFake <deedlefake@users.noreply.github.com>
2024-12-23 22:50:51 -0500

    add `@impl true`

--- lib/proto.ex
+++ lib/proto.ex
@@ -19,18 +19,21 @@ defmodule Ex9P.Proto do
     end
   end
 
+  @impl true
   def deserialize(100, <<msize::4*8-little, rest::binary>>) do
     {version, ""} = deserialize_binary(rest)
     data = %{msize: msize, version: version}
     {:tversion, data}
   end
 
+  @impl true
   def deserialize(101, <<msize::4*8-little, rest::binary>>) do
     {version, ""} = deserialize_binary(rest)
     data = %{msize: msize, version: version}
     {:rversion, data}
   end
 
+  @impl true
   def deserialize(102, <<afid::8*4-little, rest::binary>>) do
     {uname, rest} = deserialize_binary(rest)
     {aname, ""} = deserialize_binary(rest)
@@ -38,27 +41,32 @@ defmodule Ex9P.Proto do
     {:tauth, data}
   end
 
+  @impl true
   def deserialize(103, <<data::13*8-binary>>) do
     {aqid, ""} = QID.deserialize(data)
     data = %{aqid: aqid}
     {:rauth, data}
   end
 
+  @impl true
   def serialize(:tversion, %{msize: msize, version: version}) do
     data = <<msize::4*8-little, serialize_binary(version)::binary>>
     {100, data}
   end
 
+  @impl true
   def serialize(:rversion, %{msize: msize, version: version}) do
     data = <<msize::4*8-little, serialize_binary(version)::binary>>
     {101, data}
   end
 
+  @impl true
   def serialize(:tauth, %{afid: afid, uname: uname, aname: aname}) do
     data = <<afid::8*4-little, serialize_binary(uname)::binary, serialize_binary(aname)::binary>>
     {102, data}
   end
 
+  @impl true
   def serialize(:rauth, %{aqid: aqid}) do
     data = QID.serialize(aqid)
     {103, data}
