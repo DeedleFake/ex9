c77ef123f19fa0ad66ec968686d68dd8dce69316
DeedleFake <deedlefake@users.noreply.github.com>
2024-12-23 22:49:24 -0500

    remove macros and simplify message parsing

--- lib/message.ex
+++ lib/message.ex
@@ -3,122 +3,42 @@ defmodule Ex9P.Message do
   A single 9P message and related functionality.
   """
 
-  defstruct type: nil, tag: nil, data: nil
+  @enforce_keys [:type, :tag, :data]
+  defstruct @enforce_keys
 
-  def parse(size, <<id::8, tag::2*8-little, rest::binary>>, opts \\ []) do
-    [proto: proto] = Keyword.validate!(opts, proto: Ex9P.Proto)
+  def deserialize(size, <<id::8, tag::2*8-little, rest::binary>>, opts \\ []) do
+    %{proto: proto} = Keyword.validate!(opts, proto: Ex9P.Proto) |> Map.new()
 
     datasize = size - 4 - 1 - 2
     <<data::binary-(^datasize * 8), rest::binary>> = rest
-    type = proto.id_to_type(id)
-    msgdata = proto.binary_to_data(type, data)
-    {%__MODULE__{type: type, tag: tag, data: msgdata}, rest}
+    {type, data} = proto.deserialize(id, data)
+    {%__MODULE__{type: type, tag: tag, data: data}, rest}
   end
 
-  def to_binary(%{type: type, tag: tag, data: data}, opts \\ []) do
-    [proto: proto] = Keyword.validate!(opts, proto: Ex9P.Proto)
+  def serialize(%{type: type, tag: tag, data: data}, opts \\ []) do
+    %{proto: proto} = Keyword.validate!(opts, proto: Ex9P.Proto) |> Map.new()
 
-    id = proto.type_to_id(type)
-    msgdata = proto.data_to_binary(type, data)
-    size = 4 + 1 + 2 + byte_size(msgdata)
-    <<size::4*8-little, id::8, tag::2*8-little, msgdata::binary>>
+    {id, data} = proto.serialize(type, data)
+    size = 4 + 1 + 2 + byte_size(data)
+    <<size::4*8-little, id::8, tag::2*8-little, data::binary>>
+  end
+
+  def deserialize_binary(<<size::2*8-little, data::binary>>) do
+    <<data::(^size)*8-binary, rest::binary>> = data
+    {data, rest}
+  end
+
+  def serialize_binary(data) when is_binary(data) do
+    <<byte_size(data)::2*8-little, data::binary>>
   end
 
   defmodule Proto do
-    @moduledoc """
-    Module with helper macros for defining protocols.
+    @type serialized_type() :: pos_integer()
+    @type serialized_data() :: binary()
+    @type type() :: atom()
+    @type data() :: term()
 
-    ## Example
-
-    This example defines a protocol module with one type. The type is
-    a `:t`, meaning that it is a client-to-server message, has a type
-    of `:example`, and a wire ID of 100. The block defines the
-    mechanisms for converting from the binary wire format to the
-    resulting Message's data field and vice versa.
-
-        defmodule ExampleProto do
-            use Ex9P.Message.Proto
-
-            type 100, texample do
-              data(<<data::8*4>>) ->
-                %{data: data}
-              binary(%{data: data}) ->
-                <<data::8*4>>
-            end
-        end
-
-    The message's direction, :t, or :r, is the first letter of the
-    name. It must be either "t" or "r".
-    """
-
-    defmacro __using__(_opts) do
-      quote do
-        import unquote(__MODULE__)
-      end
-    end
-
-    @doc """
-    Defines a message type with the given ID and type. For more info,
-    see the module documentation.
-    """
-    defmacro message(id, {type, _, _}, do: block) do
-      type = extract_dir(type)
-
-      f =
-        unless block == nil,
-          do:
-            {:__block__, [],
-             block
-             |> Enum.map(fn
-               {:->, _, [[{:data, _, [binary]} | _], block]} ->
-                 quote do
-                   def binary_to_data(unquote(type), unquote(binary)), do: unquote(block)
-                 end
-
-               {:->, _, [[{:binary, _, [data]} | _], block]} ->
-                 quote do
-                   def data_to_binary(unquote(type), unquote(data)), do: unquote(block)
-                 end
-             end)}
-
-      quote do
-        unquote(f)
-        def id_to_type(unquote(id)), do: unquote(type)
-        def type_to_id(unquote(type)), do: unquote(id)
-      end
-    end
-
-    @doc """
-    A shortcut to define a type with no body. A type defined like this
-    will always ignore its data and return `nil`.
-    """
-    defmacro message(id, type) do
-      quote do
-        message(unquote(id), unquote(type), do: nil)
-      end
-    end
-
-    @doc """
-    A convenience function to parse a variable-sized 9P field. It
-    returns a `{bytes, rest}` tuple.
-    """
-    def parse_bytes(<<size::2*8-little, rest::binary>>) when byte_size(rest) >= size do
-      <<str::binary-(^size * 8), rest::binary>> = rest
-      {str, rest}
-    end
-
-    @doc """
-    The opposite of parse_string/1. Converts a binary into 9P-encoded
-    variable-length field.
-    """
-    def to_bytes(str) when is_binary(str) do
-      <<byte_size(str)::8*2-little, str::binary>>
-    end
-
-    defp extract_dir(type) do
-      <<dir::binary-1, type::binary>> = Atom.to_string(type)
-      unless dir in ["t", "r"], do: raise(ArgumentError, "direction in :t or :r")
-      {String.to_atom(dir), String.to_atom(type)}
-    end
+    @callback deserialize(serialized_type(), serialized_data()) :: {type(), data()}
+    @callback serialize(type(), data()) :: {serialized_type(), serialized_data()}
   end
 end
--- lib/proto.ex
+++ lib/proto.ex
@@ -3,75 +3,64 @@ defmodule Ex9P.Proto do
   Protocol definition of standard 9P messages.
   """
 
-  use Ex9P.Message.Proto
+  @behaviour Ex9P.Message.Proto
+  import Ex9P.Message, only: [serialize_binary: 1, deserialize_binary: 1]
 
   defmodule QID do
-    defstruct type: nil, version: nil, path: nil
+    @enforce_keys [:type, :version, :path]
+    defstruct @enforce_keys
 
-    def parse(<<type::8, version::8*4-little, path::8*8-little>>),
-      do: %__MODULE__{type: type, version: version, path: path}
+    def deserialize(<<type::8, version::8*4-little, path::8*8-little, rest::binary>>) do
+      {%__MODULE__{type: type, version: version, path: path}, rest}
+    end
 
-    def to_binary(%{type: type, version: version, path: path}),
-      do: <<type::8, version::8*4-little, path::8*8-little>>
+    def serialize(%{type: type, version: version, path: path}) do
+      <<type::8, version::8*4-little, path::8*8-little>>
+    end
   end
 
-  message 100, tversion do
-    data(<<msize::4*8-little, rest::binary>>) ->
-      {version, _} = parse_bytes(rest)
-      %{msize: msize, version: version}
-
-    binary(%{msize: msize, version: version}) ->
-      <<msize::4*8-little, to_bytes(version)::binary>>
+  def deserialize(100, <<msize::4*8-little, rest::binary>>) do
+    {version, ""} = deserialize_binary(rest)
+    data = %{msize: msize, version: version}
+    {:tversion, data}
   end
 
-  message 101, rversion do
-    data(<<msize::4*8-little, rest::binary>>) ->
-      {version, _} = parse_bytes(rest)
-      %{msize: msize, version: version}
-
-    binary(%{msize: msize, version: version}) ->
-      <<msize::4*8-little, to_bytes(version)::binary>>
+  def deserialize(101, <<msize::4*8-little, rest::binary>>) do
+    {version, ""} = deserialize_binary(rest)
+    data = %{msize: msize, version: version}
+    {:rversion, data}
   end
 
-  message 102, tauth do
-    data(<<afid::8*4-little, rest::binary>>) ->
-      {uname, rest} = parse_bytes(rest)
-      {aname, _} = parse_bytes(rest)
-      %{afid: afid, uname: uname, aname: aname}
-
-    binary(%{afid: afid, uname: uname, aname: aname}) ->
-      <<afid::8*4-little, to_bytes(uname)::binary, to_bytes(aname)::binary>>
+  def deserialize(102, <<afid::8*4-little, rest::binary>>) do
+    {uname, rest} = deserialize_binary(rest)
+    {aname, ""} = deserialize_binary(rest)
+    data = %{afid: afid, uname: uname, aname: aname}
+    {:tauth, data}
   end
 
-  message 103, rauth do
-    data(<<data::binary-(13 * 8)>>) ->
-      %{aqid: QID.parse(data)}
-
-    binary(%{aqid: aqid}) ->
-      QID.to_binary(aqid)
+  def deserialize(103, <<data::13*8-binary>>) do
+    {aqid, ""} = QID.deserialize(data)
+    data = %{aqid: aqid}
+    {:rauth, data}
   end
 
-  message(104, tattach)
-  message(105, rattach)
-  message(107, rerror)
-  message(108, tflush)
-  message(109, rflush)
-  message(110, twalk)
-  message(111, rwalk)
-  message(112, topen)
-  message(113, ropen)
-  message(114, tcreate)
-  message(115, rcreate)
-  message(116, tread)
-  message(117, rread)
-  message(118, twrite)
-  message(119, rwrite)
-  message(120, tclunk)
-  message(121, rclunk)
-  message(122, tremove)
-  message(123, rremove)
-  message(124, tstat)
-  message(125, rstat)
-  message(126, twstat)
-  message(127, rwstat)
+  def serialize(:tversion, %{msize: msize, version: version}) do
+    data = <<msize::4*8-little, serialize_binary(version)::binary>>
+    {100, data}
+  end
+
+  def serialize(:rversion, %{msize: msize, version: version}) do
+    data = <<msize::4*8-little, serialize_binary(version)::binary>>
+    {101, data}
+  end
+
+  def serialize(:tauth, %{afid: afid, uname: uname, aname: aname}) do
+    data = <<afid::8*4-little, serialize_binary(uname)::binary, serialize_binary(aname)::binary>>
+    {102, data}
+  end
+
+  def serialize(:rauth, %{aqid: aqid}) do
+    data = QID.serialize(aqid)
+    {103, data}
+  end
 end
