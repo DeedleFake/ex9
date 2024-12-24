88db9df06ab08c1a1cfec0cd01cc06e88a894773
DeedleFake <deedlefake@users.noreply.github.com>
2024-12-23 23:35:27 -0500

    completely overhaul the client

--- lib/client.ex
+++ lib/client.ex
@@ -1,62 +1,59 @@
 defmodule Ex9P.Client do
   use GenServer
 
-  def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, opts)
+  alias Ex9P.Message
+
+  def connect(address, port, opts \\ []) do
+    with {:ok, socket} <- :gen_tcp.connect(address, port, []) do
+      state = %{
+        opts: opts,
+        socket: socket,
+        control: self()
+      }
+
+      GenServer.start_link(__MODULE__, state)
+    end
+  end
+
+  def send(client, %Message{} = msg) do
+    GenServer.call(client, {:send, msg})
   end
 
   @impl true
-  def init(opts)
-
-  defmodule Listener do
-    use GenServer
-
-    def start_link(opts) do
-      GenServer.start_link(__MODULE__, opts)
-    end
-
-    def await(listener, from, tag) do
-      listener |> GenServer.cast({:await, from, tag})
-    end
-
-    @impl true
-    def init(proto: proto) do
-      {:ok, %{proto: proto, msgs: %{}, waiting: %{}}}
-    end
-
-    @impl true
-    def handle_cast({:await, from, tag}, opts) when is_map_key(opts.msgs, tag) do
-      {msg, msgs} = opts.msgs |> Map.pop(tag)
-      GenServer.reply(from, msg)
-      {:noreply, %{opts | msgs: msgs}}
-    end
-
-    @impl true
-    def handle_cast({:await, from, tag}, opts) do
-      {:noreply, %{opts | waiting: %{opts.waiting | tag => from}}}
-    end
-
-    @impl true
-    def handle_cast({:msg, msg}, opts) do
-      # TODO
-    end
-  end
-
-  def start_link(opts) do
-    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  def init(state) do
+    {:ok, state}
   end
 
   @impl true
-  def init(socket: socket, proto: proto) do
-    {:ok, lis} = Listener.start_link(socket: socket, proto: proto)
-    {:ok, %{socket: socket, proto: proto, listener: lis}}
-  end
-
-  @impl true
-  def handle_call({:request, msg}, from, state) do
-    state.socket |> :gen_tcp.send(state.proto.to_binary(msg))
-    state.listener |> Listener.await(from, msg.tag)
-
+  def handle_continue({:send, msg}, state) do
+    data = Message.serialize(msg)
+    :ok = :gen_tcp.send(state.socket, data)
     {:noreply, state}
+  end
+
+  @impl true
+  def handle_call({:send, msg}, _from, state) do
+    # TODO: Handle things like having been disconnected.
+    {:reply, :ok, state, {:continue, {:send, msg}}}
+  end
+
+  @impl true
+  def handle_info({:tcp, socket, <<size::4*8-little, data::binary>>}, %{socket: socket} = state) do
+    {msg, ""} = Message.deserialize(size, data, state.opts)
+    Kernel.send(state.control, {Ex9P, self(), msg})
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({:tcp_closed, socket}, %{socket: socket} = state) do
+    # TODO: Handle disconnections.
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info({:tcp_error, socket, reason}, %{socket: socket} = state) do
+    # TODO: Handle errors.
+    dbg(reason)
+    {:ok, state}
   end
 end
