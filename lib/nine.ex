ac6b1b24efd859718b33b7a8b3e987d9f2ed28d7
DeedleFake <deedlefake@users.noreply.github.com>
2024-12-25 13:56:51 -0500

    use macros for message definitions again

--- lib/nine.ex
+++ lib/nine.ex
@@ -1,3 +1,29 @@
+defmodule Ex9P.Nine.DSL do
+  @moduledoc false
+
+  defmacro defmessage(name, type_id, do: block) do
+    quote do
+      defmodule unquote(name) do
+        @behaviour __MODULE__
+        @callback decode(binary()) :: %__MODULE__{}
+        @callback encode(%__MODULE__{}) :: iodata()
+
+        @nofid (1 <<< 32) - 1
+
+        def type_id(), do: unquote(type_id)
+        unquote(block)
+      end
+
+      @impl true
+      def decode(unquote(type_id), data), do: unquote(name).decode(data)
+
+      @impl true
+      def encode(%unquote(name){} = message),
+        do: {unquote(type_id), unquote(name).encode(message)}
+    end
+  end
+end
+
 defmodule Ex9P.Nine do
   @moduledoc """
   Protocol definition of standard 9P messages.
@@ -6,8 +32,7 @@ defmodule Ex9P.Nine do
   use Ex9P.Proto
 
   import Bitwise
-
-  @nofid (1 <<< 32) - 1
+  import __MODULE__.DSL
 
   @opaque fid() :: non_neg_integer()
 
@@ -31,25 +56,47 @@ defmodule Ex9P.Nine do
     end
   end
 
-  defmodule Tversion do
+  defmessage Tversion, 100 do
     use TypedStruct
 
     typedstruct enforce: true do
       field :msize, pos_integer()
       field :version, binary()
     end
+
+    @impl true
+    def decode(<<msize::4*8-little, rest::binary>>) do
+      {version, ""} = decode_binary(rest)
+      %Tversion{msize: msize, version: version}
+    end
+
+    @impl true
+    def encode(%Tversion{msize: msize, version: version}) do
+      [<<msize::4*8-little>>, encode_binary(version)]
+    end
   end
 
-  defmodule Rversion do
+  defmessage Rversion, 101 do
     use TypedStruct
 
     typedstruct enforce: true do
       field :msize, pos_integer()
       field :version, binary()
     end
+
+    @impl true
+    def decode(<<msize::4*8-little, rest::binary>>) do
+      {version, ""} = decode_binary(rest)
+      %Rversion{msize: msize, version: version}
+    end
+
+    @impl true
+    def encode(%Rversion{msize: msize, version: version}) do
+      [<<msize::4*8-little>>, encode_binary(version)]
+    end
   end
 
-  defmodule Tauth do
+  defmessage Tauth, 102 do
     use TypedStruct
 
     typedstruct enforce: true do
@@ -57,25 +104,59 @@ defmodule Ex9P.Nine do
       field :uname, binary()
       field :aname, binary()
     end
+
+    @impl true
+    def decode(<<afid::8*4-little, rest::binary>>) do
+      {uname, rest} = decode_binary(rest)
+      {aname, ""} = decode_binary(rest)
+      %Tauth{afid: afid, uname: uname, aname: aname}
+    end
+
+    @impl true
+    def encode(%Tauth{afid: afid, uname: uname, aname: aname}) do
+      [<<afid::8*4-little>>, encode_binary(uname), encode_binary(aname)]
+    end
   end
 
-  defmodule Rauth do
+  defmessage Rauth, 103 do
     use TypedStruct
 
     typedstruct enforce: true do
       field :aqid, QID.t()
     end
+
+    @impl true
+    def decode(<<data::13*8-binary>>) do
+      {aqid, ""} = QID.decode(data)
+      %Rauth{aqid: aqid}
+    end
+
+    @impl true
+    def encode(%Rauth{aqid: aqid}) do
+      QID.encode(aqid)
+    end
   end
 
-  defmodule Rerror do
+  defmessage Rerror, 107 do
     use TypedStruct
 
     typedstruct enforce: true do
       field :ename, binary()
     end
+
+    @impl true
+    def decode(<<ename::binary>>) do
+      {ename, ""} = decode_binary(ename)
+      %Rerror{ename: ename}
+    end
+
+    @impl true
+    def encode(%Rerror{ename: ename}) do
+      encode_binary(ename)
+    end
   end
 
-  defmodule Tattach do
+  defmessage Tattach, 104 do
     use TypedStruct
 
     typedstruct enforce: true do
@@ -84,30 +165,80 @@ defmodule Ex9P.Nine do
       field :uname, binary()
       field :aname, binary()
     end
+
+    @impl true
+    def decode(<<fid::4*8-little, afid::4*8-little, rest::binary>>) do
+      afid = with @nofid <- afid, do: :nofid
+      {uname, rest} = decode_binary(rest)
+      {aname, ""} = decode_binary(rest)
+      %Tattach{fid: fid, afid: afid, uname: uname, aname: aname}
+    end
+
+    @impl true
+    def encode(%Tattach{fid: fid, afid: afid, uname: uname, aname: aname}) do
+      afid = with :nofid <- afid, do: @nofid
+
+      [
+        <<fid::4*8-little, afid::4*8-little>>,
+        encode_binary(uname),
+        encode_binary(aname)
+      ]
+    end
   end
 
-  defmodule Rattach do
+  defmessage Rattach, 105 do
     use TypedStruct
 
     typedstruct enforce: true do
       field :qid, QID.t()
     end
+
+    @impl true
+    def decode(data) do
+      {qid, ""} = QID.decode(data)
+      %Rattach{qid: qid}
+    end
+
+    @impl true
+    def encode(%Rattach{qid: qid}) do
+      QID.encode(qid)
+    end
   end
 
-  defmodule Tflush do
+  defmessage Tflush, 108 do
     use TypedStruct
 
     typedstruct enforce: true do
       field :oldtag, Ex9P.Proto.tag()
     end
+
+    @impl true
+    def decode(<<oldtag::2*8-little>>) do
+      %Tflush{oldtag: oldtag}
+    end
+
+    @impl true
+    def encode(%Tflush{oldtag: oldtag}) do
+      <<oldtag::2*8-little>>
+    end
   end
 
-  defmodule Rflush do
+  defmessage Rflush, 109 do
     defstruct []
     @type t() :: %__MODULE__{}
+
+    @impl true
+    def decode("") do
+      %Rflush{}
+    end
+
+    @impl true
+    def encode(%Rflush{}) do
+      ""
+    end
   end
 
-  defmodule Twalk do
+  defmessage Twalk, 110 do
     use TypedStruct
 
     typedstruct enforce: true do
@@ -115,13 +246,38 @@ defmodule Ex9P.Nine do
       field :newfid, Ex9P.Nine.fid()
       field :wname, [binary()]
     end
+
+    @impl true
+    def decode(<<fid::4*8-little, newfid::4*8-little, rest::binary>>) do
+      {wname, ""} = decode_list(rest, &decode_binary/1)
+      %Twalk{fid: fid, newfid: newfid, wname: wname}
+    end
+
+    @impl true
+    def encode(%Twalk{fid: fid, newfid: newfid, wname: wname}) do
+      [
+        <<fid::4*8-little, newfid::4*8-little>>,
+        encode_list(wname, &encode_binary/1)
+      ]
+    end
   end
 
-  defmodule Rwalk do
+  defmessage Rwalk, 111 do
     use TypedStruct
 
     typedstruct enforce: true do
       field :wqid, [QID.t()]
+    end
+
+    @impl true
+    def decode(data) when is_binary(data) do
+      {wqid, ""} = decode_list(data, &QID.decode/1)
+      %Rwalk{wqid: wqid}
+    end
+
+    @impl true
+    def encode(%Rwalk{wqid: wqid}) do
+      encode_list(wqid, &QID.encode/1)
     end
   end
 
@@ -143,149 +299,4 @@ defmodule Ex9P.Nine do
   # Rwstat  127
   # Topenfd 98
   # Ropenfd 99
-
-  @impl true
-  def decode(100, <<msize::4*8-little, rest::binary>>) do
-    {version, ""} = decode_binary(rest)
-    %Tversion{msize: msize, version: version}
-  end
-
-  @impl true
-  def decode(101, <<msize::4*8-little, rest::binary>>) do
-    {version, ""} = decode_binary(rest)
-    %Rversion{msize: msize, version: version}
-  end
-
-  @impl true
-  def decode(102, <<afid::8*4-little, rest::binary>>) do
-    {uname, rest} = decode_binary(rest)
-    {aname, ""} = decode_binary(rest)
-    %Tauth{afid: afid, uname: uname, aname: aname}
-  end
-
-  @impl true
-  def decode(103, <<data::13*8-binary>>) do
-    {aqid, ""} = QID.decode(data)
-    %Rauth{aqid: aqid}
-  end
-
-  @impl true
-  def decode(107, <<ename::binary>>) do
-    {ename, ""} = decode_binary(ename)
-    %Rerror{ename: ename}
-  end
-
-  @impl true
-  def decode(104, <<fid::4*8-little, afid::4*8-little, rest::binary>>) do
-    afid = with @nofid <- afid, do: :nofid
-    {uname, rest} = decode_binary(rest)
-    {aname, ""} = decode_binary(rest)
-    %Tattach{fid: fid, afid: afid, uname: uname, aname: aname}
-  end
-
-  @impl true
-  def decode(105, data) when is_binary(data) do
-    {qid, ""} = QID.decode(data)
-    %Rattach{qid: qid}
-  end
-
-  @impl true
-  def decode(108, <<oldtag::2*8-little>>) do
-    %Tflush{oldtag: oldtag}
-  end
-
-  @impl true
-  def decode(109, "") do
-    %Rflush{}
-  end
-
-  @impl true
-  def decode(110, <<fid::4*8-little, newfid::4*8-little, rest::binary>>) do
-    {wname, ""} = decode_list(rest, &decode_binary/1)
-    %Twalk{fid: fid, newfid: newfid, wname: wname}
-  end
-
-  @impl true
-  def decode(111, data) when is_binary(data) do
-    {wqid, ""} = decode_list(data, &QID.decode/1)
-    %Rwalk{wqid: wqid}
-  end
-
-  @impl true
-  def encode(%Tversion{msize: msize, version: version}) do
-    data = [<<msize::4*8-little>>, encode_binary(version)]
-    {100, data}
-  end
-
-  @impl true
-  def encode(%Rversion{msize: msize, version: version}) do
-    data = [<<msize::4*8-little>>, encode_binary(version)]
-    {101, data}
-  end
-
-  @impl true
-  def encode(%Tauth{afid: afid, uname: uname, aname: aname}) do
-    data = [<<afid::8*4-little>>, encode_binary(uname), encode_binary(aname)]
-    {102, data}
-  end
-
-  @impl true
-  def encode(%Rauth{aqid: aqid}) do
-    data = QID.encode(aqid)
-    {103, data}
-  end
-
-  @impl true
-  def encode(%Rerror{ename: ename}) do
-    data = encode_binary(ename)
-    {107, data}
-  end
-
-  @impl true
-  def encode(%Tattach{fid: fid, afid: afid, uname: uname, aname: aname}) do
-    afid = with :nofid <- afid, do: @nofid
-
-    data =
-      [
-        <<fid::4*8-little, afid::4*8-little>>,
-        encode_binary(uname),
-        encode_binary(aname)
-      ]
-
-    {104, data}
-  end
-
-  @impl true
-  def encode(%Rattach{qid: qid}) do
-    data = QID.encode(qid)
-    {105, data}
-  end
-
-  @impl true
-  def encode(%Tflush{oldtag: oldtag}) do
-    data = <<oldtag::2*8-little>>
-    {108, data}
-  end
-
-  @impl true
-  def encode(%Rflush{}) do
-    {109, ""}
-  end
-
-  @impl true
-  def encode(%Twalk{fid: fid, newfid: newfid, wname: wname}) do
-    data =
-      [
-        <<fid::4*8-little, newfid::4*8-little>>,
-        encode_list(wname, &encode_binary/1)
-      ]
-
-    {110, data}
-  end
-
-  @impl true
-  def encode(%Rwalk{wqid: wqid}) do
-    data = encode_list(wqid, &QID.encode/1)
-    {111, data}
-  end
 end
