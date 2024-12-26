137ad1d4c0b68163149cae36c54ed12cab1eb6d6
DeedleFake <deedlefake@users.noreply.github.com>
2024-12-25 22:02:53 -0500

    do the handshake automatically at client startup

--- lib/nine/client.ex
+++ lib/nine/client.ex
@@ -1,11 +1,6 @@
 defmodule Ex9P.Nine.Client do
-  use GenServer
-
-  alias Ex9P.Conn
   alias Ex9P.Nine
   alias Ex9P.Nine.DirEntry
-
-  @version "9P2000"
 
   @opaque t() :: GenServer.server()
 
@@ -19,27 +14,16 @@ defmodule Ex9P.Nine.Client do
     end
   end
 
+  @doc false
+  defdelegate child_spec(spec), to: __MODULE__.GenServer
+
   def start_link(opts) do
     {server_opts, opts} = Keyword.split(opts, [:name])
-    GenServer.start_link(__MODULE__, opts, server_opts)
+    GenServer.start_link(__MODULE__.GenServer, opts, server_opts)
   end
 
-  defp request(client, msg, opts \\ []) do
-    GenServer.call(client, {:request, msg, opts})
-  end
-
-  @spec handshake(t(), pos_integer()) ::
-          {:ok, pos_integer()} | {:error, Exception.t()} | {:error, :unsupported_version}
-  def handshake(client, msize) do
-    rsp = request(client, %Nine.Tversion{msize: msize, version: @version}, notag: true)
-
-    with %Nine.Rversion{msize: msize, version: version} <- rsp,
-         ^version <- @version do
-      {:ok, msize}
-    else
-      err when is_exception(err) -> {:error, err}
-      @version -> {:error, :unsupported_version}
-    end
+  defp request(client, msg) do
+    GenServer.call(client, {:request, msg})
   end
 
   @spec attach(t(), String.t(), String.t(), File.t() | nil) ::
@@ -78,59 +62,5 @@ defmodule Ex9P.Nine.Client do
     else
       err when is_exception(err) -> {:error, err}
     end
-  end
-
-  @impl true
-  def init(opts) do
-    opts = Keyword.validate!(opts, [:address, port: 0, connect_opts: []])
-    {:ok, conn} = Conn.connect(opts[:address], opts[:port], opts[:connect_opts])
-
-    state = %{
-      conn: conn,
-      tags: %{},
-      next_tag: 1,
-      next_fid: 1
-    }
-
-    {:ok, state}
-  end
-
-  @impl true
-  def handle_call({:request, msg, opts}, from, state) do
-    {msg, state} =
-      if opts[:notag] do
-        state = put_in(state.tags[:notag], from)
-        {msg, state}
-      else
-        {tag, state} = get_and_update_in(state.next_tag, &{&1, &1 + 1})
-        state = put_in(state.tags[tag], from)
-        {{tag, msg}, state}
-      end
-
-    :ok = Conn.send(state.conn, msg)
-    {:noreply, state}
-  end
-
-  @impl true
-  def handle_call(:next_fid, _from, state) do
-    {fid, state} = get_and_update_in(state.next_fid, &{&1, &1 + 1})
-    {:reply, fid, state}
-  end
-
-  @impl true
-  def handle_info({Ex9P, conn, {tag, msg}}, %{conn: conn} = state) do
-    {from, state} = pop_in(state.tags[tag])
-    GenServer.reply(from, msg)
-
-    {:noreply, state}
-  end
-
-  @impl true
-  def handle_info({Ex9P, conn, msg}, %{conn: conn} = state) do
-    from = state.tags[:notag]
-    GenServer.reply(from, msg)
-
-    state = %{state | tags: %{}}
-    {:noreply, state}
   end
 end
---
+++ lib/nine/client/gen_server.ex
@@ -0,0 +1,79 @@
+defmodule Ex9P.Nine.Client.GenServer do
+  use GenServer
+
+  alias Ex9P.Conn
+  alias Ex9P.Nine
+
+  @version "9P2000"
+
+  @impl true
+  def init(opts) do
+    opts = Keyword.validate!(opts, [:address, port: 0, connect_opts: [], msize: 8192])
+    {:ok, conn} = Conn.connect(opts[:address], opts[:port], opts[:connect_opts])
+
+    state = %{
+      conn: conn,
+      tags: %{},
+      msize: [],
+      next_tag: 1,
+      next_fid: 1
+    }
+
+    :ok = Conn.send(state.conn, %Nine.Tversion{msize: opts[:msize], version: @version})
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_call({:request, msg}, from, state) do
+    if is_integer(state.msize) do
+      state = request(state, msg, from)
+      {:noreply, state}
+    else
+      state = update_in(state.msize, fn msize -> [{msg, from} | msize] end)
+      {:noreply, state}
+    end
+  end
+
+  @impl true
+  def handle_call(:next_fid, _from, state) do
+    {fid, state} = get_and_update_in(state.next_fid, &{&1, &1 + 1})
+    {:reply, fid, state}
+  end
+
+  @impl true
+  def handle_info({Ex9P, conn, %Nine.Rversion{msize: msize}}, %{conn: conn, msize: queue} = state) do
+    state = %{state | msize: msize}
+
+    state =
+      Enum.reduce(queue, state, fn {msg, from}, state ->
+        request(state, msg, from)
+      end)
+
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({Ex9P, conn, {tag, msg}}, %{conn: conn} = state) do
+    {from, state} = pop_in(state.tags[tag])
+    GenServer.reply(from, msg)
+
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({Ex9P, conn, msg}, %{conn: conn} = state) do
+    from = state.tags[:notag]
+    GenServer.reply(from, msg)
+
+    state = %{state | tags: %{}}
+    {:noreply, state}
+  end
+
+  defp request(state, msg, from) do
+    {tag, state} = get_and_update_in(state.next_tag, &{&1, &1 + 1})
+    state = put_in(state.tags[tag], from)
+
+    :ok = Conn.send(state.conn, {tag, msg})
+    state
+  end
+end
